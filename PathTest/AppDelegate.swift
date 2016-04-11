c595be15d031ccfec72fabef32b76d40e7fc0fff
Henk van der Spek <henk@vanmezelf.nl>
2016-04-11 12:22:14 +0200

    Initial check-in

---
+++ PathTest/AppDelegate.swift
@@ -0,0 +1,16 @@
+//
+//  AppDelegate.swift
+//  PathTest
+//
+//  Created by Henk van der Spek on 10/03/16.
+//  Copyright © 2016 treatwell. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+}
+
